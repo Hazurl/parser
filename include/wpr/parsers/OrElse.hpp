5a69d29f69b34d0acbe9253502b0f072a963384c
Hazurl <ricoujules69@gmail.com>
2019-09-22 13:03:14 +0200

    Add an or_else variant for functions

--- include/wpr/parsers/OrElse.hpp
+++ include/wpr/parsers/OrElse.hpp
@@ -13,6 +13,11 @@ auto or_else_helper(E) {
     return V;
 }
 
+template<typename E, auto F>
+auto or_elsef_helper(E) {
+    return F();
+}
+
 }
 
 template<auto V>
@@ -21,6 +26,12 @@ struct OrElse {};
 template<auto V>
 constexpr OrElse<V> or_else;
 
+template<auto F>
+struct OrElseF {};
+
+template<auto F>
+constexpr OrElseF<F> or_elsef;
+
 
 namespace details {
 
@@ -28,6 +39,12 @@ template<auto V, typename P>
 struct GetTransformer<OrElse<V>, P> {
     using error_t = details::error_of_t<details::result_type_t<P>>;
     using type = Handle<P, or_else_helper<error_t, V> >;
+};
+
+template<auto F, typename P>
+struct GetTransformer<OrElseF<F>, P> {
+    using error_t = details::error_of_t<details::result_type_t<P>>;
+    using type = Handle<P, or_elsef_helper<error_t, F> >;
 };
 
 }
