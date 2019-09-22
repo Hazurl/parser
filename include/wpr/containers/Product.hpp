a390b437648a6354e3acee1afc9263ce52cc74c4
Hazurl <ricoujules69@gmail.com>
2019-09-22 13:05:23 +0200

    Add as_tuple to product

--- include/wpr/containers/Product.hpp
+++ include/wpr/containers/Product.hpp
@@ -3,7 +3,14 @@
 #include <ostream>
 #include <tuple>
 
+<<<<<<< refs/remotes/origin/parser-v2:include/wpr/containers/Product.hpp
 namespace wpr {
+=======
+#include <ws/parser2/Details.hpp>
+
+
+namespace ws::parser2 {
+>>>>>>> Add as_tuple to product:include/ws/parser2/containers/Product.hpp
 
 /*
     Details
@@ -16,6 +23,9 @@ namespace product_details {
 
 template<typename...Ts, std::size_t...Is>
 std::ostream& print_product_helper(std::ostream& os, Product<Ts...> const& prod, std::index_sequence<Is...>);
+
+template<typename...Ts, std::size_t...Is>
+std::string describe_product_helper(Product<Ts...> const& prod, std::index_sequence<Is...>);
 
 template<typename...Ts, std::size_t...Is>
 bool equal_product_helper(Product<Ts...> const& lhs, Product<Ts...> const& rhs, std::index_sequence<Is...>);
@@ -36,12 +46,36 @@ struct Product : std::tuple<Ts...> {
     Product() = default;
     Product(Ts... ts) : std::tuple<Ts...>(std::move(ts)...) {};
     Product(from_tuple_t, std::tuple<Ts...> ts) : std::tuple<Ts...>(ts) {};
+
+    std::tuple<Ts...> const& as_tuple() const& {
+        return static_cast<std::tuple<Ts...> const&>(*this);
+    }
+
+    std::tuple<Ts...> & as_tuple() & {
+        return static_cast<std::tuple<Ts...> &>(*this);
+    }
+
+    std::tuple<Ts...> && as_tuple() && {
+        return static_cast<std::tuple<Ts...> &&>(*this);
+    }
 };
 
 template<>
 struct Product<> : std::tuple<> {
     Product() = default;
     Product(from_tuple_t, std::tuple<> ts) : std::tuple<>(ts) {};
+
+    std::tuple<> const& as_tuple() const& {
+        return static_cast<std::tuple<> const&>(*this);
+    }
+
+    std::tuple<> & as_tuple() & {
+        return static_cast<std::tuple<> &>(*this);
+    }
+
+    std::tuple<> && as_tuple() && {
+        return static_cast<std::tuple<> &&>(*this);
+    }
 };
 
 template<typename...Ts> Product(Ts...) -> Product<Ts...>;
@@ -59,11 +93,18 @@ template<typename...Ts> Product(from_tuple_t, std::tuple<Ts...>) -> Product<Ts..
 template<typename...Ts>
 struct Describe<Product<Ts...>> {
     std::string operator()(Product<Ts...> const& prod) {
-        return std::apply([] (auto const&...ts) {
-            return (std::string{ "(" } + ... + describe(ts)) + ")";
-        }, static_cast<std::tuple<Ts...> const&>(prod));
+        return product_details::describe_product_helper(prod, std::make_index_sequence<sizeof...(Ts)>{});
     }
 };
+
+namespace product_details {
+
+    template<typename...Ts, std::size_t...Is>
+    std::string describe_product_helper(Product<Ts...> const& prod, std::index_sequence<Is...>) {
+        return (std::string{ "(" } + ... + (std::string(Is == 0 ? "" : ", ") + describe(std::get<Is>(prod)))) + ")";
+    }
+
+}
 
 
 
